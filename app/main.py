8f9559a4b60fd3a48f95bb777c546f1520d4fed8
Nopporn Phantawee <n.phantawee@gmail.com>
2019-05-02 15:28:55 +0700

    sf

--- app/main.py
+++ app/main.py
@@ -1,19 +1,25 @@
 from flask import Flask,render_template,jsonify,request,g
-import pyodbc
+# import pyodbc
+import sqlalchemy
 import pandas as pd
 app = Flask(__name__)
 
 ############################################################################################################
 ####################################### Database connection ################################################
 ############################################################################################################
-
-connstring = 'secret'
-cnxn = pyodbc.connect(connstring)
-cursor = cnxn.cursor()
+# connection_string = "mssql+pyodbc://<user>@<server-host>:<password>@<server-host>.database.windows.net:1433/<database>?driver=ODBC+Driver+13+for+SQL+Server"
+# engine = sqlalchemy.engine.create_engine(connection_string)
+# connection = engine.connect()
+# result = connection.execute("select max(id) from atable")
+# for row in result:
+#     print(row)
+# connection.close()
 
 def get_db():
     if 'db' not in g:
-        g.db = pyodbc.connect(connstring)
+        connection_string = "mssql+pyodbc://<user>@<server-host>:<password>@<server-host>.database.windows.net:1433/<database>?driver=ODBC+Driver+13+for+SQL+Server"
+        engine = sqlalchemy.engine.create_engine(connection_string)
+        connection = engine.connect()
     return g.db
 
 @app.teardown_appcontext
@@ -142,7 +148,26 @@ def extractJSON(strKey,json_input):
   # print('varout is {}'.format(varout))
   return varout
 
-
+# upload and show content of spreadsheet file
+@app.route("/upload", methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        print(request.files['file'])
+        f = request.files['file']
+        data_xls = pd.read_excel(f)
+        return data_xls.to_html()
+    return '''
+    <!doctype html>
+    <title>Upload an excel file</title>
+    <h1>Excel file upload (csv, tsv, csvz, tsvz only)</h1>
+    <form action="" method=post enctype=multipart/form-data>
+    <p><input type=file name=file><input type=submit value=Upload>
+    </form>
+    '''
+@app.route("/export", methods=['GET'])
+def export_records():
+    # TODO[2019-05-02 15:07]: add code to output excel file for user downloading
+    return 
 ############################################################################################################
 ####################################### Start FLASK app ####################################################
 ############################################################################################################
